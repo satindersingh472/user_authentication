dabd1067d141dcf59d5189db8d210d95cc4d3dd7
satinder <satindersingh472@gmail.com>
2022-07-30 14:56:45 -0600

    finished

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
+// go home function wil take back to the home page
 function go_home(details){
     location.href = `home.html`;
 }
+// success login will set cookies value from response data and redirect to home page
 function success_login(response) {
     let token_value = response[`data`][`token`];
     Cookies.set(`token`, token_value);
@@ -8,9 +10,13 @@ function success_login(response) {
     setTimeout(go_home,2000);
 
 }
+// failure login will display msg if any error
 function failure_login(error) {
     box[`innerHTML`] = `<h2>Invalid email or password</h2>`;
 }
+// function login will perform axios request 
+// it will first grab value from the form and store them in seperate variables
+// then the post method will use those variables and data object key value pairs
 function login(details) {
     let email_value = document.getElementById(`email`)[`value`];
     let password_value = document.getElementById(`password`)[`value`];
@@ -24,6 +30,8 @@ function login(details) {
         }
     }).then(success_login).catch(failure_login);
 }
+// box is the div inside index html document
 let box = document.getElementById(`box`);
+// submit button is also inside index document it is there to execute the login function
 let submit_button = document.getElementById(`submit`);
 submit_button.addEventListener(`click`, login);
--- js/home.js
+++ js/home.js
@@ -1,29 +1,42 @@
+// cookies_value will store the value of token cookies
 let cookies_value = Cookies.get(`token`);
+// remove_cookies function will remove the cookies with logout button clicked 
+function remove_cookies(details) {
+    Cookies.remove(`token`);
+}
+// function success color will give name and year and div with background color same as the color name
 function success_color(response) {
+    // the loop will go thru the data inside data inside response and give back the name year and color
     for (let i = 0; i < response[`data`][`data`].length; i++) {
         box[`innerHTML`] += `<div class="color_description">
     <h2 style="color:${response[`data`][`data`][i][`color`]};">${response[`data`][`data`][i][`name`]}</h2>
     <h2>This color was created in ${response[`data`][`data`][i][`year`]}</h2>   
     </div>
-    <div class="color_box" style="background-color: ${response[`data`][`data`][i][`color`]}">
-        
-    </div>
+
+    <div class="color_box" style="background-color: ${response[`data`][`data`][i][`color`]};"> The color in the background is ${response[`data`][`data`][i][`name`]}</div>
     `;
     }
 }
+// if something gets wrong with the request then this function will dislay the error 
 function failure_color(error) {
-    box[`innerHTML`] = `<h2>Error Showing the output</h2>`;
+    box[`innerHTML`] = `<h2>Error in showing the output</h2>`;
 }
+// displat color function will run the axios request if button is user is logged in correctly 
+// the user will be logged in and their will be a cookies in a jar that will have token for authentication
 function display_color(details) {
-
     axios.request({
         url: `https://reqres.in/api/unknown`
     }).then(success_color).catch(failure_color);
 
 }
 let box = document.getElementById(`box`);
+// if there is cookies with token of authentication then if will execute the display color function
 if (cookies_value) {
     display_color();
+    // else it will ask the user to login
 } else {
     box[`innerHTML`] = `<h2>You should  <a href = "index.html">login</a> see some colors here!</h2>`;
 }
+// button remove will call the remove cookies function with the help of click event
+let button_remove = document.getElementById(`remove`);
+button_remove.addEventListener(`click`, remove_cookies);
